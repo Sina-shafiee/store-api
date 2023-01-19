948b59a601d5d5cfec321e25dc9b28c862f11c82
Sina <alisinashafieezada@gmail.com>
2023-01-19 15:43:49 +0330

    simple query filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,22 @@
+const Product = require('../models/product');
+
 const getAllProducts = async (req, res, next) => {
-  res.status(200).json({ data: 'data' });
+  const { featured, company } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+
+  const products = await Product.find(queryObj);
+
+  if (!products.length) {
+    throw new Error('404 not found');
+  }
+  res.status(200).json({ data: products });
 };
 
 module.exports = { getAllProducts };
